ccebef7484aa5e31d0b4646b2bad8ab8c2433a33
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-14 03:17:51 +0100

    Removed ESLint comment

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const app = require('./app.js');
 require('dotenv').config();
 
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 app.listen(port, () => {
-	// eslint-disable-next-line no-console
 	console.log(`Listening on port ${port}`);
 });
